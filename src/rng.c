591fcc851676252fe42c03bf8ab819155440e05e
Hugo Benedicto Tanidi <88760617+kingdicto@users.noreply.github.com>
2022-11-04 16:36:33 +0700

    Add files via upload

---
+++ src/rng.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include "mesinkarakter.h"
+
+void rng(){
+    srand(time(NULL));
+    int r = rand()%101;
+    int masukan;
+
+    printf("RNG Telah dimulai. Uji keberuntungan Anda dengan menebak X.\n");
+    printf("%d\n", r);
+    printf("Tebakan: ");
+    scanf("%d", &masukan);
+    while (masukan != r){
+        if (masukan < r){
+            printf("Lebih besar\n");
+        }
+        else{
+            printf("Lebih kecil\n");
+        }
+        printf("Tebakan: ");
+        scanf("%d", &masukan);
+    }
+    printf("Ya, X adalah ");
+    printf("%d.", masukan);
+}
