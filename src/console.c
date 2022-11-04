5b121d3f94c10bbe6ede97e9c7220c50dd65fb3f
samuel-eric <30772879+samuel-eric@users.noreply.github.com>
2022-11-04 17:24:07 +0700

    add creategame

--- src/console.c
+++ src/console.c
@@ -69,6 +69,15 @@ void STARTBNMO(ArrayDin *GameBNMO)
     printf("File konfigurasi sistem berhasil dibaca. BNMO berhasil dijalankan.\n");
 }
 
+void CREATEGAME (ArrayDin *GameBNMO) {
+    printf("Masukkan nama game yang akan ditambahkan: ");
+    char *namaGame;
+    namaGame = (char *) malloc (50 * sizeof(char));
+    scanf("%s", namaGame);
+    InsertLast(GameBNMO, namaGame);
+    printf("Game berhasil ditambahkan\n");
+}
+
 void LISTGAME (ArrayDin GameBNMO)
 {
     int i;
