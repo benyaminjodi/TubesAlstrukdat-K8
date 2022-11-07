c34fe40a10158715f5875baa14ea21cb8fa593dc
benyaminjodi <benyaminj312@gmail.com>
2022-11-07 21:57:39 +0700

    update mesinkata

--- src/ADT/mesinkata/mesinkata.c
+++ src/ADT/mesinkata/mesinkata.c
@@ -131,7 +131,7 @@ void CopyCommand()
           Jika panjang kata melebihi NMax, maka sisa kata "dipotong" */
 {
     int i = 0;
-    while ((CC != ENTER) && i != NMax)
+    while ((CC != ENTER) && i != NMax && (CC != BLANK))
     {
         CurrentCommand.TabWord[i] = CC;
         ADVC();
