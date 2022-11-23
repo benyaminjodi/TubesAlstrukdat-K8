754f74c51b4bf02c1e6f9d55ffe4c50e8dfb57d0
CarissaZ <92920009+CarissaZ@users.noreply.github.com>
2022-11-24 05:39:47 +0700

    add hangman.h

---
+++ src/hangman.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <time.h>
+#include "boolean.h"
+#include "ADT/linkedlist/linkList.h"
+#include "ADT/mesinkarakter/mesinkarakter.h"
+#include "console.h"
+#include "ADT/arraydinamis/arraydin.h"
+
+void HANGMAN ();
