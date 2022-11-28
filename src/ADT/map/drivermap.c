012d361e15641097af7313ea612564c0852f52b5
benyaminjodi <benyaminj312@gmail.com>
2022-11-28 13:33:42 +0700

    revisi

--- src/ADT/map/drivermap.c
+++ src/ADT/map/drivermap.c
@@ -5,14 +5,14 @@ int main()
     Map M;
     char A,B,C;
     CreateEmptyMap(&M);
-    Insert(&M, &A, 2);
-    Insert(&M, &B, 3);
-    Insert(&M, &C, 4);
+    InsertMap(&M, &A, 2);
+    InsertMap(&M, &B, 3);
+    InsertMap(&M, &C, 4);
     printf("%d\n", Value(M, &A));
     printf("%d\n", Value(M, &B));
     printf("%d\n", Value(M, &C));
     
-    Delete(&M, &A);
+    DeleteMap(&M, &A);
     printf("%d\n", Value(M, &A));
     printf("%d\n", Value(M, &B));
     printf("%d\n", Value(M, &C));
